366cbaeba935913d7ddba2c8a307d2a04e258fb0
Shirvil Sheth <96599193+ssheth75@users.noreply.github.com>
2023-08-18 02:29:19 -0400

    compress photos

--- styling.js
+++ styling.js
@@ -24,29 +24,22 @@ function updateBorder(section) {
 }
 
 function openEmailClient() {
-  // Define the email address and subject
   var emailAddress = "shirvil@umich.edu";
   var subject = "";
 
-  // Construct the mailto link
   var mailtoLink =
     "mailto:" + emailAddress + "?subject=" + encodeURIComponent(subject);
-
-  // Open the user's default email client
   window.location.href = mailtoLink;
 }
 
 
 function scrollToSection(scrollDistance) {
-    // Calculate the distance you want to scroll to (in pixels)
-    // Use smooth scrolling to the specified distance
     window.scrollTo({
         top: scrollDistance,
         behavior: 'smooth'
     });
 }
 
-// script.js
 document.addEventListener('DOMContentLoaded', function() {
     const fadeContent = document.getElementById('content');
     fadeContent.classList.add('fade-in');
